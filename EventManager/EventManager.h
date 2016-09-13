f2f94dc9b8d9f1fd963b4cb04b5cc76cc0055c86
Chris <krzysiekzabicki@interia.pl>
2016-09-13 13:01:56 +0200

    Creating EventManager based on : 'SFML Game Development by Example'

---
+++ EventManager/EventManager.h
@@ -0,0 +1,30 @@
+#pragma once
+
+using Events = std::vector<std::pair<EventType, EventInfo>;
+
+enum class EventType
+{
+	KeyDown = sf::Event::KeyPressed,
+	KeyUp = sf::Event::KeyReleased,
+	MButtonDown = sf::Event::MouseButtonPressed,
+	MButtonUp = sf::Event::MouseButtonReleased,
+	MouseWheel = sf::Event::MouseWheelMoved,
+	WindowResized = sf::Event::Resized,
+	GainedFocus = sf::Event::GainedFocus,
+	LostFocus = sf::Event::LostFocus,
+	MouseEntered = sf::Event::MouseEntered,
+	MouseLeft = sf::Event::MouseLeft,
+	Closed = sf::Event::Closed,
+	TextEntered = sf::Event::TextEntered,
+	Keyboard = sf::Event::Count + 1, Mouse, Joystick
+};
+
+struct EventInfo
+{
+	EventInfo() { code = 0; }
+	EventInfo(int event) { code = event; }
+	union 
+	{
+		int code;
+	}
+}
